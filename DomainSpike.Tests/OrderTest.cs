288f87a84228e557ae3e7dfd6e0fe92bf2e1e4f4
SchoeCR <159347003+SchoeCR@users.noreply.github.com>
2025-02-19 21:38:57 +1000

    Added happy path unit test.

--- DomainSpike.Tests/OrderTest.cs
+++ DomainSpike.Tests/OrderTest.cs
@@ -1,3 +1,5 @@
+using Xunit.Sdk;
+
 namespace DomainSpike.Tests;
 
 public class OrderTest
@@ -12,5 +14,23 @@ public class OrderTest
         
         // Act & Assert 
         Assert.Throws<ArgumentException>(() => new Order(orderId, orderItems, address));
+    }
+
+    [Fact]
+
+    public void OrderWithIDItemsAndAddressIsValid()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        var orderItems = new List<OrderItem>();
+        orderItems.Add(new OrderItem(Guid.NewGuid(), Guid.NewGuid(), 10, new Money(125m, "USD")));
+        Address address = new Address("76 Hideaway Street", "Calgary", "T3C2X4", "Canada");
+
+        // Act & Assert 
+        var order = new Order(orderId, orderItems, address);
+        Assert.Equal(orderId, order.OrderId);
+        Assert.Single(order.Items);
+        Assert.Equal(1250m, order.TotalAmount.Amount);
+
     }
 }
